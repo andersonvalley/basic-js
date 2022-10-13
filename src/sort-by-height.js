ecf6d0d45b155e357a6a2433702365881d079f4b
Andrey Mironov <a.mironov@lucky-team.pro>
2022-10-13 10:27:22 +0300

    feat: complet 3 tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const indexes = []
+  const sorted = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      indexes.push(i)
+    } else {
+      sorted.push(arr[i])
+    }
+  }
+
+  sorted.sort((a, b) => a - b)
+
+  for (let i = 0; i < indexes.length; i++) {
+    sorted.splice(indexes[i], 0, -1)
+  }
+
+  return sorted
 }
 
 module.exports = {
-  sortByHeight
-};
+  sortByHeight,
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  throw new NotImplementedError('Not implemented')
   // remove line with error and write your code here
+  let name
+
+  if (!date) return 'Unable to determine the time of year!'
+
+  const month = typeof date.getMonth === 'function' ? date : ''
+
+  if (month === '') {
+    throw new Error('Invalid date!')
+  }
+
+  if (date.getFullYear() < 1970) {
+    throw new Error('Invalid date!')
+  }
+
+  const season = [
+    { name: 'winter', months: [11, 0, 1] },
+    { name: 'summer', months: [5, 6, 7] },
+    { name: 'autumn', months: [8, 9, 10] },
+    { name: 'spring', months: [2, 3, 4] },
+  ]
+
+  season.forEach((item) => {
+    if (item.months.includes(month.getMonth())) {
+      name = item.name
+    }
+  })
+
+  return name
 }
 
 module.exports = {
-  getSeason
-};
+  getSeason,
+}
