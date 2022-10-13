57e013b95bc00b3b6efd34df4c8d73b3e8556978
Andrey Mironov <a.mironov@lucky-team.pro>
2022-10-13 23:22:27 +0300

    feat: complet one task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,7 +8,7 @@ const HALF_LIFE_PERIOD = 5730
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
  *
  * @param {String} sampleActivity string representation of current activity
- * @return {Number | Boolean} calculated age in years or false
+ * @return {+| Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
@@ -18,8 +18,16 @@ const HALF_LIFE_PERIOD = 5730
  *
  */
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented')
-  // remove line with error and write your code here
+  const MODERN_ACTIVITY = 15
+  const HALF_LIFE_PERIOD = 5730
+
+  if (typeof sampleActivity !== 'string') return false
+
+  if (!+sampleActivity) return false
+
+  if (+sampleActivity > 0 && +sampleActivity < 15) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD))
+  } else return false
 }
 
 module.exports = {
